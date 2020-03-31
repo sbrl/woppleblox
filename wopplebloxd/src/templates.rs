079466e02479609a89f973e361639aefca6cbaf2
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2020-03-31 12:08:34 +0100

    Integrate existing templates as static files. Next up: translatability with fluent.
    ....We're not going to get caught out like last time

--- wopplebloxd/src/templates.rs
+++ wopplebloxd/src/templates.rs
@@ -2,8 +2,25 @@ use yarte::Template;
 
 #[derive(Template)]
 #[template(path = "index.html")]
-// #[template(path = "index.html", mode =" html-min")]
-pub struct MainTemplate<'a> {
+// #[template(path = "index.html", mode ="html-min")]
+pub struct Main<'a> {
     pub title: &'a str,
     pub content: &'a str
+}
+
+
+#[derive(Template)]
+#[template(path = "post.html")]
+pub struct Post<'a> {
+    pub username: &'a str,
+    pub content: &'a str,
+    pub logo_url: &'a str
+}
+
+#[derive(Template)]
+#[template(path = "user-profile.html")]
+pub struct UserProfile<'a> {
+    pub username: &'a str,
+    pub posts: &'a str,
+    pub logo_url: &'a str
 }
