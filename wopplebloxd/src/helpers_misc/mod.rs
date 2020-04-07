c419ae15f422a30bddd71d1d6379c788a17596c8
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2020-04-07 13:45:36 +0100

    NormalResult: The type arguments are all wrong, but we haven't managed to fix them yet.

--- wopplebloxd/src/helpers_misc/mod.rs
+++ wopplebloxd/src/helpers_misc/mod.rs
@@ -1,11 +1,11 @@
-pub trait NormalResult {
-    fn make_normal<T>(&self) -> std::Result<T, String>;
+pub trait NormalResult<T> {
+    fn make_normal<T>(&self) -> std::result::Result<T, String>;
 }
 
-impl NormalResult for rusqlite::Result {
-    fn make_normal<T>(&self) -> std::Result<T, String> {
+impl NormalResult<T> for rusqlite::Result<T> {
+    fn make_normal<T>(&self) -> std::result::Result<T, String> {
         match self {
-            rusqlite::Result::Ok(val) => std::Result::Ok(val),
+            rusqlite::Result::Ok(val) => std::result::Result::Ok(val),
             rusqlite::Result::Err(error) =>  std::Result::Err(error.to_tring())
         }
     }
